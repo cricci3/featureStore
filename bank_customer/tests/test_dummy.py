c2f823c005439b506687ae6d19c0df502809453b
dami013 <damianoficara@gmail.com>
2024-06-21 17:42:34 +0200

    fix pipeline

--- bank_customer/tests/test_dummy.py
+++ bank_customer/tests/test_dummy.py
@@ -2,25 +2,25 @@ import pytest
 from feast import FeatureStore
 # Inizializza il client del Feature Store con il percorso corretto al tuo repository
 feature_store = FeatureStore(repo_path=".")
+feature_service = feature_store.get_feature_service("user_activity")
 
 def test_data_loading():
     try:
         # Specifica una query di esempio per controllare i dati
         data = feature_store.get_online_features(
-            feature_refs=["df1_feature_view:Gender", "df2_feature_view:CreditScore"],
+            feature_refs=feature_service,
             entity_rows=[{"USER_ID": 1}]
         )
         assert data is not None
         assert len(data) > 0
     except Exception as e:
         pytest.fail(f"Data loading failed: {e}")
-import time
 
 def test_feature_validation():
     consumer_id = 1
     entity_rows = [{"USER_ID": consumer_id}]
     features = feature_store.get_online_features(
-        feature_refs=["df1_feature_view:Gender", "df2_feature_view:CreditScore"],
+        feature_refs=feature_service,
         entity_rows=entity_rows
     )
     for feature_name in ["df1_feature_view:Gender", "df2_feature_view:CreditScore"]:
